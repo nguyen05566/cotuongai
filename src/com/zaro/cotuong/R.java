6af60d002d1ceb53b1eee80b955b503870710a4c
nguyen05566 <javjp606@gmail.com>
2026-06-20 15:00:13 +0700

    feat: add R.java - resources

---
+++ src/com/zaro/cotuong/R.java
@@ -0,0 +1,24 @@
+package com.zaro.cotuong;
+public final class R {
+    public static final class id {
+        public static final int boardView=0x7f010000;
+        public static final int titleText=0x7f010001;
+        public static final int turnText=0x7f010002;
+        public static final int statusText=0x7f010003;
+        public static final int btnNew=0x7f010004;
+        public static final int btnUndo=0x7f010005;
+        public static final int btnHint=0x7f010006;
+        public static final int btnFlip=0x7f010007;
+        public static final int btnSelectRed=0x7f010008;
+        public static final int btnSelectBlack=0x7f010009;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f020000;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher=0x7f030000;
+    }
+    public static final class string {
+        public static final int app_name=0x7f040000;
+    }
+}
